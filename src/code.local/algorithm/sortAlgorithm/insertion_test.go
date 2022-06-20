1fbd0ca54aa1ae97ff8aa7ecd493fd1b3bc87107
yangwenbo <yangwenbo@dgg.net>
2022-06-20 18:56:51 +0800

    feat：add merge_sort

--- src/code.local/algorithm/sortAlgorithm/insertion_test.go
+++ src/code.local/algorithm/sortAlgorithm/insertion_test.go
@@ -8,6 +8,12 @@ import "testing"
 
 var arr = []int{6, 5, 3, 1, 8, 7, 2, 4}
 
+func BenchmarkMergeSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mergeSort(arr)
+	}
+}
+
 func BenchmarkInsertionSort(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		arr = InsertionSort(arr)
@@ -29,5 +35,11 @@ func BenchmarkBubbleSort(b *testing.B) {
 func BenchmarkSelectionSort(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		selectionSort(arr)
+	}
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		quickSort(arr, 0, len(arr)-1)
 	}
 }
